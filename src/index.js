be5ad46c9dbb95f565eee97c7f46edaf09c6a3e1
annchapon <annchapon@gmail.com>
2018-09-21 18:34:50 +0300

    implement solveEquation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+  var array = equation.split(' ');
+  var results = [];
+  
+  var a = Number(array[0]);
+  var b = Number(array[3] + array[4]);
+  var c = Number(array[7] + array[8]);
+  
+  var d = b*b - 4*a*c;
+  
+  if ( d > 0) {
+    var d_sqr = Math.sqrt(d);
+    var x1 = (-b + d_sqr) / (2*a);
+    var x2 = (-b - d_sqr) / (2*a);
+    results.push(Math.round(x1));
+    results.push(Math.round(x2));
+  }
+  
+  if ( d === 0) {
+    var x = (-b) / (2*a);
+    results.push(Math.round(x));
+  }
+  
+  results.sort(function(left, right) {
+    if ( left > right ) return 1;
+    if ( left < right ) return -1;
+  });
+  
+  return results;
+
 }
